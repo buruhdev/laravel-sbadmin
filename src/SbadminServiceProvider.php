8d0f21cd43515b6c3121997d08507f88cfa4b7fb
aziyan99 <rajaazian08@gmail.com>
2023-02-02 15:16:03 +0700

    publish meta tags component

--- src/SbadminServiceProvider.php
+++ src/SbadminServiceProvider.php
@@ -3,8 +3,6 @@
 namespace Aziyan99\ThemeSbadmin;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Blade;
-use Aziyan99\ThemeSbadmin\View\Components\MetaTagsComponent;
 
 class SbadminServiceProvider extends ServiceProvider
 {
@@ -17,25 +15,16 @@ class SbadminServiceProvider extends ServiceProvider
     {
         $this->publishes([
             __DIR__.'/resources/views' => base_path('resources/views')
-        ], 'sbadmin-layouts');
+        ], 'sbadmin-theme');
 
         $this->publishes([
             __DIR__.'/public' => public_path()
-        ], 'sbadmin-assets');
+        ], 'sbadmin-theme');
 
-
-        $this->registerViews();
-        $this->registerViewComponents();
-
-    }
-
-    private function registerViews(): void
-    {
-        $this->loadViewsFrom(__DIR__ . '/resources/views/components', 'components');
-    }
-
-    private function registerViewComponents(): void
-    {
-        Blade::component('sbadmin-meta-tags', MetaTagsComponent::class);
+        $this->
+        
+        $this->publishes([
+            __DIR__.'/app/View/Components' => base_path('/app/View/Components')
+        ], 'sbadmin-theme');
     }
 }
