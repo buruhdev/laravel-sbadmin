5954ba6ac4a1c00a0c2b9db24f36829a4cf83cab
aziyan99 <rajaazian08@gmail.com>
2023-02-02 16:02:53 +0700

    renaming

--- src/resources/views/components/sbadmin/left-menu-component.blade.php
+++ src/resources/views/components/sbadmin/left-menu-component.blade.php
@@ -1,24 +1,24 @@
-<nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+<nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+    <div class="sb-sidenav-menu">
+        <div class="nav">
 
-    <a class="navbar-brand ps-3" href="javascript:void(0);">{{ $appName }}</a>
-
-    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="javascript:void(0);">
-        <i class="fas fa-bars"></i>
-    </button>
-
-    <div class="d-none d-md-inline-block ms-auto me-0 me-md-3 my-2 my-md-0"></div>
-
-    <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="fas fa-user fa-fw"></i>
+            <a class="nav-link" href="javascript:void(0);">
+                <div class="sb-nav-link-icon"><i class="fas fa-home"></i></div>
+                Home
             </a>
-            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="javascript:void(0);">Settings</a></li>
-                <li><a class="dropdown-item" href="javascript:void(0);">Activity Log</a></li>
-                <li><hr class="dropdown-divider" /></li>
-                <li><a class="dropdown-item" href="javascript:void(0);">Logout</a></li>
-            </ul>
-        </li>
-    </ul>
+            <a class="nav-link" href="javascript:void(0);">
+                <div class="sb-nav-link-icon"><i class="fas fa-clock"></i></div>
+                Recent
+            </a>
+            <a class="nav-link" href="javascript:void(0);">
+                <div class="sb-nav-link-icon"><i class="fas fa-trash"></i></div>
+                Trash
+            </a>
+
+        </div>
+    </div>
+    <div class="sb-sidenav-footer">
+        <div class="small">Logged in as:</div>
+        Start Bootstrap
+    </div>
 </nav>
